d38bd1aed1dba0948c97ba46b25c4a48938bd404
Amanda Ducrou <aducrou@expedia.com>
2018-05-23 16:12:18 +1000

    First Component

--- server/server.js
+++ server/server.js
@@ -15,14 +15,14 @@ const app = express();
 
 const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+  throw new Error('You must provide a Mongo URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI, {useMongoClient: true});
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+    .once('open', () => console.log('Connected to Mongo instance.'))
+    .on('error', error => console.log('Error connecting to Mongo:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
